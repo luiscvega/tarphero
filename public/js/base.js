5918ad94af2375eddd3968902386230cf07228d5
Luis Vega <luiscvega@gmail.com>
2013-04-19 21:19:34 +0800

    initial dropzone stuff

--- public/js/base.js
+++ public/js/base.js
@@ -1,9 +1,23 @@
 $(function() {
     $("#image-upload").ajaxForm({
-        dataType: "json", 
+        dataType: "json",
+
+        beforeSubmit: function() {
+            $("#upload-multiple").toggleClass("uploading");
+        },
 
         success: function(data) {
-          var images = data.images;
+            var images = data.images;
+
+            $("#upload-multiple").toggleClass("uploading");
+
+            $("#customize-product").append(
+                "<img src='" + images[0].url + "' />"
+            );
         }
     });
+
+  Dropzone.options.myAwesomeDropzone = {
+    paramName: "images[]"
+  };
 });
